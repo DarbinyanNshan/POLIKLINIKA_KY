8b2dc4e8f9841d3f08fb4aa7685821cd9849aa41
DarbinyanNshan <119871844+DarbinyanNshan@users.noreply.github.com>
2024-10-28 21:20:33 +0400

    poliklinika

--- poliklinika/src/pages/error404/index.jsx
+++ poliklinika/src/pages/error404/index.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import "./style.css";
+import error from "../../assets/images/404-error.jpg"
+import { useNavigate } from "react-router-dom";
 
 export const Error = () => {
+    const navigate = useNavigate();
 
-
+    const onClick = () => {
+        navigate('/');
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
     return (
-        <div>
-            <h1>Contact</h1>
+        <div className="error">
+            <img src={error} alt="error" />
+            <h2>Oops! It looks like you are lost ...!</h2>
+            <button onClick={onClick}>Go back to Home</button>
         </div>
     )
 }
